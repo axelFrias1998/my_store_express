ef5a3558db1a921392b331b61e8e2a57dfcdad40
Axel Frías Hernández <axel.frias257@gmail.com>
2021-11-30 21:45:05 -0600

    Dos endpoint de prueba. Uno normal y otro json

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const { request, response } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get('/', (request, response) => {
 	response.send("Hello world from Express!");
+});
+
+app.get('/test', (request, response) => {
+	response.send("Hola test");
+});
+
+app.get('/mi_flaquita', (request, response) => {
+	response.json({
+		latigo: "Lizeth",
+		enculado: "Axel",
+		quiereBoda: true,
+		quierePerro: true,
+		cuantosPerros: 2
+	});
 });
 
 app.listen(port, () => {
