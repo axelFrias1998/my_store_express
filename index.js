de3529e61308c923314cd794ee8ddf9e9843665e
Axel Frías Hernández <axel.frias257@gmail.com>
2021-11-30 21:33:55 -0600

    Configuración de express y primera ruta

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My app');
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+	response.send("Hello world from Express!");
+});
+
+app.listen(port, () => {
+	console.log("Running on: " + port);
+});
