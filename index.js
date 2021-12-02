6c2ccf26bafdb27132bedf613d8f7ffb24349daf
Axel Frías Hernández <axel.frias257@gmail.com>
2021-12-01 22:38:09 -0600

    Inicio de delegación de responsabilidades

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { request, response } = require("express");
 const express = require("express");
+const faker = require("faker");
+
 const app = express();
 const port = 3000;
 
@@ -8,20 +9,22 @@ app.get("/", (request, response) => {
 });
 
 app.get("/products", (request, response) => {
-	response.json([
-		{
-			name: "Calzón",
-			price: 100
-		},
-		{
-			name: "Tanga",
-			price: 20
-		},
-		{
-			name: "Cruz católica",
-			price: 200
-		}
-	]);
+	const { size } = request.query;
+	const limit = size || 100;
+	const products = [];
+	for (let i = 0; i < limit; i++) {
+		products.push({
+			name: faker.commerce.productName(),
+			price: parseInt(faker.commerce.price()),
+			image: faker.image.imageUrl()
+		});
+	}
+	response.json(products);
+});
+
+//Todo lo específico (filter) va antes de lo dinámico, para evitar confusiones de routing
+app.get("/products/filter", (request, response) => {
+	response.send("Filter");
 });
 
 app.get("/products/:id", (request, response) => {
@@ -39,6 +42,20 @@ app.get("/categories/:categoryId/products/:productId", (request, response) => {
 		categoryId,
 		productId
 	});
+});
+
+//Parámetros desde query
+app.get("/users", (request, response) => {
+	const { limit, offset } = request.query;
+	if(limit && offset){
+		response.json({
+			limit,
+			offset
+		});
+	}
+	else{
+		response.send("sin parámetros");
+	}
 });
 
 app.get("/mi_flaquita", (request, response) => {
