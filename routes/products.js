d8f02abb7621f305a5831f7cc4056c7b8f1db101
Axel Frías Hernández <axel.frias257@gmail.com>
2021-12-06 23:02:20 -0600

    Añadida librería boom con status code

--- routes/products.js
+++ routes/products.js
@@ -4,53 +4,76 @@ const ProductsService = require("../services/products");
 const router = express.Router();
 const service = new ProductsService();
 
-router.get("/", (request, response) => {
+router.get("/", async (request, response) => {
 	//?size=100
 	//const { size } = request.query;
-	const products = service.find();
+	const products = await service.find();
 	response.status(200).json(products);
 });
 
 //Todos los parámetros enviados o por query se reciben por string
-router.get("/:id", (request, response) => {
-	const { id } = request.params;
-	const product = service.findOne(id);
-	if(product){
-		response.status(200).json(product);
-	}
-	else{
-		response.status(404).json({
-			message: "Not found"
-		});
+router.get("/:id", async (request, response, next) => {
+	try {
+		const { id } = request.params;
+		const product = await service.findOne(id);
+		response.json(product);
+	} catch (error) {
+		next(error);
 	}
 });
 
-router.post("/", (request, response) => {
+/*Funcionamiento de un middleware (similar a un pipe)
+
+La salida de uno, es la entrada de otro
+function(request, result, next){
+	if (something){
+		response.send('end');
+	}
+	else{
+		next();
+	}
+	if (error){
+		response.status(500).json({error});
+	}
+	else{
+		next();
+	}
+}*/
+
+router.post("/", async (request, response) => {
 	const body = request.body;
-	const newProduct = service.create(body);
+	const newProduct = await service.create(body);
 	response.status(201).json({
 		message: "Created",
 		data: newProduct
 	});
 });
 
-router.put("/:id", (request, response) => {
-	const { id } = request.params;
-	const body = request.body;
-	const product = service.update(id, body);
-	response.json(product);
+router.put("/:id", async (request, response, next) => {
+	try {
+		const { id } = request.params;
+		const body = request.body;
+		const product = await service.update(id, body);
+		response.json(product);
+	} catch (error) {
+		next(error);
+	}
 });
 
-router.patch("/:id", (request, response) => {
-	const { id } = request.params;
-	const body = request.body;
-	const product = service.update(id, body);
-	response.json(product);
+router.patch("/:id", async (request, response, next) => {
+	try {
+		const { id } = request.params;
+		const body = request.body;
+		const product = await service.update(id, body);
+		response.json(product);
+	} catch (error) {
+		next(error);
+	}
 });
 
-router.delete("/:id", (request, response) => {
+router.delete("/:id", async (request, response) => {
 	const { id } = request.params;
-	const result = service.delete(id);
+	const result = await service.delete(id);
 	response.json(result);
 });
 
--- services/products.js
+++ services/products.js
@@ -1,5 +1,6 @@
 //Lógica nivel transaccional va aquí
 const faker = require("faker");
+const boom = require("@hapi/boom");
 
 class ProductsService {
 
@@ -16,7 +17,8 @@ class ProductsService {
 				id: faker.datatype.uuid(),
 				name: faker.commerce.productName(),
 				price: parseInt(faker.commerce.price()),
-				image: faker.image.imageUrl()
+				image: faker.image.imageUrl(),
+				isBlock: faker.datatype.boolean()
 			});
 		}
 	}
@@ -32,17 +34,30 @@ class ProductsService {
 	}
 
 	async find(){
-		return this.products;
+		//Retraso de 5 segundos
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve(this.products);
+			}, 5000);
+		})
 	}
 
 	async findOne(id){
-		return this.products.find(item => item.id === id);
+		const product = this.products.find(item => item.id === id);
+		if(!product){
+			throw boom.notFound("Product not found");
+		}
+		//Restricción por regla de negocio. Si el producto está bloqueado
+		if(product.isBlock){
+			throw boom.conflict("Product is blocked");
+		}
+		return product;
 	}
 
 	async update(id, changes){
 		const index = this.products.findIndex(item => item.id === id);
 		if(index === -1){
-			throw new Error("Product not found");
+			throw boom.notFound("Product not found");
 		}
 		//Ajusta lo anterior y lo nuevo
 		const product = this.products[index];
@@ -56,7 +71,7 @@ class ProductsService {
 	async delete(id){
 		const index = this.products.findIndex(item => item.id === id);
 		if(index === -1){
-			throw new Error("Product not found");
+			throw boom.notFound("Product not found");
 		}
 		this.products.splice(index, 1);
 		return { id };
